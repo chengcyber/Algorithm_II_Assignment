3c923782e13bf5d0d0be117a50f4831af0f9a566
liu.cheng <liu.cheng@sfit.shfe.com.cn>
2016-03-23 02:06:38 +0800

    mod add comments to SAP, WordNet.

--- src/ii_01_WordNet/SAP.java
+++ src/ii_01_WordNet/SAP.java
@@ -6,13 +6,24 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
-	// constructor takes a digraph (not necessarily a DAG)
+	/**
+	 * Constructor: SAP
+	 * Usage: SAP sap = new SAP(G);
+	 * @param G a instance of Digraph Class
+	 */
 	public SAP(Digraph G)
 	{
 		this.G = new Digraph(G);
 	}
 
-	// length of shortest ancestral path between v and w; -1 if no such path
+	/**
+	 * Computes the length of shortest ancestral path between v and w; -1 if no such path
+	 * Method: length(v, w)
+	 * Usage: int length = sap(v, w);
+	 * @param v int type number if out of index throw exception.
+	 * @param w int type number if out of index throw exception.
+	 * @return the length of shortest ancestral path between v and w; -1 if no such path
+	 */
 	public int length(int v, int w)
 	{
 		validateVertex(v);
@@ -31,7 +42,14 @@ public class SAP {
         return result;
 	}
 
-	// a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+	/**
+	 * a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+	 * Method: ancestor(v, w)
+	 * Usage: int ancestor = sap.ancestor(v, w);
+	 * @param v int type number if out of index throw exception.
+	 * @param w int type number if out of index throw exception.
+	 * @return ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+	 */
 	public int ancestor(int v, int w)
 	{
 		validateVertex(v);
@@ -50,8 +68,16 @@ public class SAP {
 		}
         return ancResult;
 	}
-
-	// length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+	
+	/**
+	 * length of shortest ancestral path between any vertex in v and any vertex in w;
+	 *  -1 if no such path
+	 * Method: length(v, w)
+	 * Usage: int length = sap.length(v, w);
+	 * @param v int type number if out of index throw exception.
+	 * @param w int type number if out of index throw exception.
+	 * @return length of shortest ancestral path between any vertex in v and any vertex in w
+	 */
 	public int length(Iterable<Integer> v, Iterable<Integer> w)
 	{
 		for(int x : v)
@@ -72,7 +98,14 @@ public class SAP {
 		return result;
 	}
 
-	// a common ancestor that participates in shortest ancestral path; -1 if no such path
+	/**
+	 * a common ancestor that participates in shortest ancestral path; -1 if no such path
+	 * Method: ancestor(v, w);
+	 * Usage: int ancestor = sap.ancestor(v, w);
+	 * @param v int type number if out of index throw exception.
+	 * @param w int type number if out of index throw exception.
+	 * @return a common ancestor that participates in shortest ancestral path
+	 */
 	public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
 	{
 		for(int x : v)
@@ -94,7 +127,10 @@ public class SAP {
         return ancResult;
 	}
 
-	// do unit testing of this class
+	/**
+	 * Testing main function to SAP class.
+	 * @param args [0]the Digraph context file.
+	 */
 	public static void main(String[] args)
 	{
 		In in = new In(args[0]);
@@ -113,11 +149,18 @@ public class SAP {
 
 
 	/* Private Section */
+	
+	/**
+	 * validates whether int v is out of range.If so, throw exception
+	 * Method: validateVertex(v)
+	 * Usage: validateVertex(v);
+	 * @param v a int type number.
+	 */
 	private void validateVertex(int v) {
 		if (v < 0 || v >= G.V())
             throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (G.V()-1));
 	}
 
 	/* Private Instance variables */
-	private Digraph G;
+	private Digraph G;		/* a instance of Digraph Class */ 
 }
--- src/ii_01_WordNet/WordNet.java
+++ src/ii_01_WordNet/WordNet.java
@@ -11,6 +11,23 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class WordNet {
 	// constructor takes the name of the two input files
+	/**
+	 * Constructor : WordNet
+	 * Usage: WordNet wordnet = WordNet(synsets, hypernyms);
+	 * @param synsets 
+	 * 		List of noun synsets. The file synsets.txt lists all the (noun) synsets in WordNet. 
+	 * 		The first field is the synset id (an integer), the second field is the synonym set 
+	 * 		(or synset), and the third field is its dictionary definition (or gloss). For example,
+	 * 		the line
+	 *  		36,AND_circuit AND_gate,a circuit in a computer that fires only when all of its inputs fire
+	 * 		means that the synset { AND_circuit, AND_gate } has an id number of 36 and it's gloss is a circuit in a computer that fires only when all of its inputs fire. The individual nouns that comprise a synset are separated by spaces (and a synset element is not permitted to contain a space). The S synset ids are numbered 0 through S − 1; the id numbers will appear consecutively in the synset file.
+	 * @param hypernyms
+	 * 		List of hypernyms. The file hypernyms.txt contains the hypernym relationships: The first 
+	 * 		field is a synset id; subsequent fields are the id numbers of the synset's hypernyms. For
+	 * 		 example, the following line
+	 *			164,21012,56099
+	 *		means that the the synset 164 ("Actifed") has two hypernyms: 21012 ("antihistamine") and 56099 ("nasal_decongestant"), representing that Actifed is both an antihistamine and a nasal decongestant. The synsets are obtained from the corresponding lines in the file synsets.txt.
+	 */
 	public WordNet(String synsets, String hypernyms) {
 		this.wordMap = new HashMap<String, Bag<Integer> >();
 		In inSynsets = new In(synsets);
@@ -18,12 +35,6 @@ public class WordNet {
 		int maxNumber = 0;
 		while (inSynsets.hasNextLine()) {
 			String[] tokens = inSynsets.readLine().split(delims);
-			
-			// DEBUG
-//			for (int i = 0; i < tokens.length; i++) {
-//				System.out.println(tokens[i]);
-//			}
-			
 			int wordNumber = Integer.parseInt(tokens[0]);
 			maxNumber = maxInteger(maxNumber, wordNumber);
 			if (!wordMap.containsKey(tokens[1])) {
@@ -42,8 +53,6 @@ public class WordNet {
 			int v = Integer.parseInt(tokens[0]);
 			for (int i = 1; i < tokens.length; i++) {
 				int w = Integer.parseInt(tokens[i]);
-				//DEBUG
-//				System.out.println("adding:" + v + " -> " + w);
 				G.addEdge(v, w);
 			}
 		}
@@ -51,24 +60,41 @@ public class WordNet {
 		sap = new SAP(G);
 	}
 
-	// returns all WordNet nouns
+	/**
+	 * all WordNet nouns
+	 * Usage: Iterable<String> itr = wordnet.nouns();
+	 * @return all WordNet nouns
+	 */
 	public Iterable<String> nouns() {
 		return wordMap.keySet();
 	}
 
-	// is the word a WordNet noun?
+	/**
+	 * Usage: if (isNoun(word)) . . .
+	 * @param word
+	 * @return true if word is a WordNet noun. otherwise false;
+	 */
 	public boolean isNoun(String word) {
 		if (wordMap.isEmpty()) return false;
 		return wordMap.containsKey(word);
 	}
 
-	// distance between nounA and nounB (defined below)
+	/**
+	 * Usage: int distance = wordnet.distance(nounA, nounB);
+	 * @param nounA String type value
+	 * @param nounB String type value
+	 * @return distance between nounA and nounB (defined below)
+	 */
 	public int distance(String nounA, String nounB) {
 		return sap.length(wordMap.get(nounA), wordMap.get(nounB));
 	}
 
-	// a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-	// in a shortest ancestral path (defined below)
+	/**
+	 * Usage: String anc = wordnet.sap(nounA, nounB);
+	 * @param nounA String type value
+	 * @param nounB String type value
+	 * @return the common ancestor of nounA and nounB in a shortest ancestral path
+	 */
 	public String sap(String nounA, String nounB) {
 		String result = "";
 		int ancestor = sap.ancestor(wordMap.get(nounA), wordMap.get(nounB));
@@ -86,21 +112,14 @@ public class WordNet {
 		return result;
 	}
 
-	// do unit testing of this class
+	/**
+	 * Testing main function
+	 * @param args [0] synsets.txt , [1] hypernyms.txt
+	 */
 	public static void main(String[] args) {
 		if(args.length < 2)
 			throw new java.lang.NullPointerException("lack of arguments!");
 		WordNet wordnet = new WordNet(args[0], args[1]);
-		
-		// DEBUG
-//		for (String word : wordnet.nouns()) {
-//			System.out.println(word);
-//		}
-//		for(int x : wordnet.G.adj(1))
-//			System.out.println(x);
-//		for(int x : wordnet.G.adj(5))
-//			System.out.println(x);
-		
 		while (!StdIn.isEmpty()) 
 		{
 			String v = StdIn.readString();
@@ -113,12 +132,18 @@ public class WordNet {
 	}
 	
 	/* Private Methods */
+	/**
+	 * Usage: int max = maxInteger(a, b);
+	 * @param a int type value
+	 * @param b int type value
+	 * @return the larger number between a and b
+	 */
 	private int maxInteger(int a, int b) {
 		return (a > b)?a:b;
 	}
 	
 	/* Private instance variables */
-	private HashMap<String, Bag<Integer> > wordMap;
-	private Digraph G;
-	private SAP sap;
+	private HashMap<String, Bag<Integer> > wordMap;		/* HashMap for noun to ints. */
+	private Digraph G;									/* instance of Digraph */
+	private SAP sap;									/* instance of SAP */
 }
